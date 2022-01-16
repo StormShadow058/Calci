b000db4a438213067bd05450ea14fabb63040661
Storm Shadow <83181647+StormShadow058@users.noreply.github.com>
2022-01-16 14:41:43 +0530

    Initial Commit

---
+++ Calci/CalciApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalciApp.swift
+//  Calci
+//
+//  Created by Vansh Maheshwari on 16/01/22.
+//
+
+import SwiftUI
+
+@main
+struct CalciApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
